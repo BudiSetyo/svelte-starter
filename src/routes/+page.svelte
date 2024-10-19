b83b0fc59e866ef7bd73085d8f439f817b34f4d3
BudiSetyo <budisetyo176@gmail.com>
2024-10-20 02:34:15 +0700

    feat: initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '@unocss/reset/tailwind.css';
+	import 'unocss';
+</script>
+
+<div class="h-full font-sans text-slate-500 dark:text-slate-400 bg-slate-50 dark:bg-slate-950">
+	<slot></slot>
+</div>
---
+++ src/routes/notes/+page.svelte
@@ -0,0 +1,3 @@
+<main>
+	<h1>This is a heading</h1>
+</main>
